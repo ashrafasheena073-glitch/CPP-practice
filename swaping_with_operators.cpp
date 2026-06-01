94ded2d2aceca833389b2b2353cd6da892686570
Asheena Ashraf <ashrafasheena073@gmail.com>
2026-06-01 21:20:40 +0500

    Add files via upload

---
+++ swaping_with_operators.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+	// swaping with operators 
+	// addition and subtraction
+	int a=18;
+	int b=19;
+	cout<<" before swaping "<< endl;
+	cout << "a="<<a <<endl;
+	cout << "b="<<b <<endl;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	cout << "after swaping "<<endl;
+	cout<< "a=" << a <<endl;
+	cout<< "b=" <<b <<endl;
+	
+
+	
+	
+return 0;	
+}
