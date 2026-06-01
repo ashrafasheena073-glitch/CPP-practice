df6ec543f43cea1b518b6534369bb917f18d69f6
Asheena Ashraf <ashrafasheena073@gmail.com>
2026-06-01 21:05:31 +0500

    Add files via upload
    From this you can check whether the number is positive or negative

---
+++ postive_negative.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std ;
+int main (){
+	
+	
+cout<< "Here you can check whether the value is positive or negative" <<endl; 
+ int n;
+ cout << "Enter the value ";
+ cin >> n;
+ if(n>0){cout<<"value is positive";
+ }
+else if(n<0){cout<<"value is negative";
+ }	
+else{cout<<"value is zero";
+ }	
+	
+	
+	return 0;
+}
